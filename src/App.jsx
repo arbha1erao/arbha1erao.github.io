9825f43c122dc4c315adbe55012d1a6c37db816d
Aditya Bhalerao <adityab7968@gmail.com>
2025-01-06 01:18:29 +0530

    Add background

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      hello world
-    </h1>
-  )
+  return <>
+    {/* Note(aditya): change background colour here */}
+    <div class="fixed -z-10 min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#cbd5e1_100%)]"></div>
+  </>
 }
