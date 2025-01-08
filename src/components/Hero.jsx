db458fc0ad71ce20a707ab0ef5f7def2763ef06b
Aditya Bhalerao <adityab7968@gmail.com>
2025-01-08 23:37:21 +0530

    Add motion to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,24 @@
 import image from "/public/aditya.png"
+import { motion } from "framer-motion"
+
 const Hero = () => {
     return (
         <div id="home" className="px-16 flex min-h-screen w-full items-center justify-center py-28 md:px-32">
 
             <div className="flex flex-col items-center justify-center gap-10 text-white">
-                <div>
+                <motion.div
+                    initial={{ y: -50, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 0.8, delay: 0.2 }}
+                >
                     <img src={image} alt="aditya" className="w-[300px] cursor-pointer rounded-full shadow-xl shadow-indigo-900 transition-all duration-300 hover:-translate-y-5 hover:scale-105 hover:shadow-2xl hover:shadow-indigo-600 md:w-[350px]" />
-                </div>
-                <div className="flex max-w-[600px] flex-col items-center justify-center gap-3 text-center">
+                </motion.div>
+
+                <motion.div
+                    initial={{ y: 50, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 0.8, delay: 0.2 }}
+                    className="flex max-w-[600px] flex-col items-center justify-center gap-3 text-center">
                     <h1 className="bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent text-5xl font-light md:text-7xl">
                         Aditya Bhalerao
                     </h1>
@@ -15,8 +26,14 @@ const Hero = () => {
                         Software Engineer
                     </h3>
                     <p className="md:text-base text-pretty text-sm text-gray-400">
+                        I'm a Software Engineer with a focus on backend development, passionate about building scalable, efficient, and reliable systems.
+                        <br /><br />
+                        I specialize in crafting APIs, managing databases, and optimizing server-side performance to deliver seamless user experiences.
+                        <br /><br />
+                        I thrive on solving complex problems and continuously improving my skills.
                     </p>
-                </div>
+                </motion.div>
+
             </div>
 
         </div>
