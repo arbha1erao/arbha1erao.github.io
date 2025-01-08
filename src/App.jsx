0cf87ba24692fb524703cbe238ece721288d3b50
Aditya Bhalerao <adityab7968@gmail.com>
2025-01-09 00:02:32 +0530

    Add tech

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Corpus from "./components/Corpus";
 export default function App() {
   return <>
     {/* Note(aditya): change background colour here */}
-    <div class="fixed -z-10 min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#cbd5e1_100%)]"></div>
+    <div className="fixed -z-10 min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#cbd5e1_100%)]"></div>
 
     <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
       <Navbar />
