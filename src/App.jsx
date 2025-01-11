78c41e11ce44027e47791920048731e881128e8d
Aditya Bhalerao <adityab7968@gmail.com>
2025-01-11 21:10:24 +0530

    Change background

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Experience from "./components/Experience";
 export default function App() {
   return <>
     {/* Note(aditya): change background colour here */}
-    <div className="fixed -z-10 min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#cbd5e1_100%)]"></div>
+    <div className="fixed -z-10 min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
 
     <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
       <Navbar />
