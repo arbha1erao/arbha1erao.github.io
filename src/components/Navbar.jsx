e096477f1f13975b061991971d897d53e7f92158
Aditya Bhalerao <adityab7968@gmail.com>
2025-01-12 03:03:07 +0530

    Navbar tweaks (still needs debugging)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { BiMenu, BiX } from "react-icons/bi";
 import { BsGithub, BsLinkedin } from "react-icons/bs"
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
 
@@ -12,28 +13,26 @@ const Navbar = () => {
 
     return (
         <nav className="fixed top-0 z-10 flex w-full items-center justify-between border-b border-b-gray-700 bg-black/70 px-16 py-6 text-white backdrop-blur-md md:justify-evenly">
-
-            <a href="#home" className="bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent opacity-80 text-3xl font-semibold transition-all duration-300 hover:opacity-100">AB</a>
+            <Link to="/" className="transition-all duration-300 opacity-80 hover:opacity-100">
+                <img src="/aditya.svg" alt="Aditya Logo" className="h-10 w-10" />
+            </Link>
 
             <ul className="hidden md:flex gap-10">
-                <a href="#home" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+                <Link to="/" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
                     <li>Home</li>
-                </a>
-                <a href="#tech" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+                </Link>
+                <Link to="/#tech" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
                     <li>Tech</li>
-                </a>
-                <a href="#experience" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+                </Link>
+                <Link to="/#experience" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
                     <li>Experience</li>
-                </a>
-                {/* <a href="#projects" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
-                    <li>Projects</li>
-                </a> */}
-                <a href="#corpus" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
-                    <li>Corpus</li>
-                </a>
-                <a href="#contact" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+                </Link>
+                <Link to="/#archive" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+                    <li>Archive</li>
+                </Link>
+                <Link to="/#contact" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
                     <li>Contact</li>
-                </a>
+                </Link>
             </ul>
 
             <ul className="hidden md:flex gap-5">
@@ -58,24 +57,21 @@ const Navbar = () => {
             {isOpen && (
                 <div className={`fixed right-0 top-[84px] flex h-screen w-1/2 flex-col items-start justify-start gap-10 border-l border-gray-800 bg-black/90 p-12 ${isOpen ? "block" : "hidden"}`}>
                     <ul className="flex flex-col gap-8">
-                        <a href="#home" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+                        <Link to="/" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
                             <li>Home</li>
-                        </a>
-                        <a href="#tech" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+                        </Link>
+                        <Link to="/#tech" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
                             <li>Tech</li>
-                        </a>
-                        <a href="#experience" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+                        </Link>
+                        <Link to="/#experience" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
                             <li>Experience</li>
-                        </a>
-                        {/* <a href="#projects" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
-                            <li>Projects</li>
-                        </a> */}
-                        <a href="#corpus" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
-                            <li>Corpus</li>
-                        </a>
-                        <a href="#contact" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+                        </Link>
+                        <Link to="/#archive" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+                            <li>Archive</li>
+                        </Link>
+                        <Link to="/#contact" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
                             <li>Contact</li>
-                        </a>
+                        </Link>
                     </ul>
                     <ul className="flex flex-wrap gap-5">
                         <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-orange-500 hover:opacity-100">
