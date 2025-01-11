64bd539e38d5e1e0a895ea8d014875e336ac6fc7
Aditya Bhalerao <adityab7968@gmail.com>
2025-01-12 02:59:47 +0530

    App.jsx all changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,48 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
 import Tech from "./components/Tech";
-import Corpus from "./components/Corpus";
+import Archive from "./components/Archive";
 import Experience from "./components/Experience";
+import ResearchPaperList from "./components/ResearchPaperList";
+import TedTalksList from "./components/TedTalksList";
 
 export default function App() {
-  return <>
-    {/* Note(aditya): change background colour here */}
-    <div className="fixed -z-10 min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-
-    <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
+  return (
+    <Router>
+      <div className="fixed -z-10 min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       <Navbar />
-      <Hero />
-      <Tech />
-      <Experience />
-        {/* <Projects /> */}
-      <Corpus />
-      <Contact />
-    </main>
-  </>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
+              <Hero />
+              <Tech />
+              <Experience />
+              <Archive />
+              <Contact />
+            </main>
+          }
+        />
+        <Route
+          path="/archive/research-papers"
+          element={
+            <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
+              <ResearchPaperList />
+            </main>
+          }
+        />
+        <Route
+          path="/archive/ted-talks"
+          element={
+            <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
+              <TedTalksList />
+            </main>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
