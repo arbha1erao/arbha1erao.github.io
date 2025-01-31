4acf9be7fd2ee74d9ef86b9c2f6b61a40f051e75
Aditya Bhalerao <adityab7968@gmail.com>
2025-01-31 21:15:40 +0530

    Add paper

--- src/components/Papershelf.jsx
+++ src/components/Papershelf.jsx
@@ -11,6 +11,11 @@ const papersList = [
     title: "Zanzibar: Google’s Consistent, Global Authorization System",
     summary: "This paper details Zanzibar, Google's scalable and globally distributed access control system, designed to provide consistent and efficient permission management at scale.",
     link: "https://research.google/pubs/zanzibar-googles-consistent-global-authorization-system/",
+  },
+  {
+    title: "Consistent Hashing and Random Trees",
+    summary: "This work laid the groundwork for modern distributed systems like Dynamo and Cassandra.",
+    link: "https://www.cs.princeton.edu/courses/archive/fall09/cos518/papers/chash.pdf",
   }
 ];
 
